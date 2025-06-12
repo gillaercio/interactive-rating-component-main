c4ab489b01450c393b13c0661324ab4737d92b3b
Gildman Laércio <laerciokarlagabm@gmail.com>
2025-06-12 04:56:34 -0300

    Button interactivity created

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,38 @@
 let state = true;
+let selectedRating = null;
+
+document.querySelectorAll('.rating-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    document.querySelectorAll('.rating-btn').forEach(btn => {
+      btn.classList.remove('selected');
+    });
+    button.classList.add('selected');
+    selectedRating = button.textContent;
+  })
+})
 
 function feedback() {
   const content = document.querySelector('#default-content');
-  const buttons = document.querySelectorAll('.rating-btn');
   const congrats = document.querySelector('#congrats-content');
   const score = document.querySelector('#score');
 
+  if (selectedRating) {
+    score.textContent = selectedRating;
+  } else {
+    score.textContent = "no rating";
+  }
+
   if (window.innerWidth < 992){
-    if (state){
-      content.style.display = "block";
-      congrats.style.display = "none";
-    } else {
-      content.style.display = "none";
-      congrats.style.display = "block";
-    }
+    content.style.display = state ? "block" : "none";
+    congrats.style.display = state ? "none" : "block";
     state = !state;
   } else if (window.innerWidth < 1201){
-    if (state){
-      content.style.display = "block";
-      congrats.style.display = "none";
-    } else {
-      content.style.display = "none";
-      congrats.style.display = "block";
-    }
+    content.style.display = state ? "block" : "none";
+    congrats.styel.display = state ? "none" : "block";
     state = !state;
   } else {
-    if (state) {
-      content.style.display = "block";
-      congrats.style.display = "none";
-    } else {
-      content.style.display = "none";
-      congrats.style.display = "block";
-    }
+    content.style.display = state ? "block" : "none";
+    congrats.style.display = state ? "none" : "block";
     state = !state;
   }
 }
